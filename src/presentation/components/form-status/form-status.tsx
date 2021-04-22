08fc33b98162253d37943ed8922aaee741c4e1dc
jpvramos <joaopaulovieiraramos@gmail.com>
2021-04-22 16:59:23 -0300

    test: ensure Login calls validation with correct email

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -4,12 +4,12 @@ import Styles from './form-status-styles.scss'
 import { Context } from '@/presentation/contexts'
 
 const FormStatus: React.FC = () => {
-  const { state, errorState } = React.useContext(Context)
+  const { state } = React.useContext(Context)
 
   return (
   <div data-testid="error-wrap" className={Styles.erroWrap}>
     { state.isLoading && <Spinner className={Styles.spinner} /> }
-    { errorState.main && <span className={Styles.error}>{errorState.main}</span> }
+    { state.mainError && <span className={Styles.error}>{state.mainError}</span> }
 
   </div>
   )
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -2,22 +2,29 @@ import React from 'react'
 import Styles from './login-styles.scss'
 import { Footer, Input, LoginHeader, FormStatus } from '@/presentation/components'
 import { Context } from '@/presentation/contexts'
+import { Validation } from '@/presentation/protocols/validations'
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+type Props = {
+  validation: Validation
+}
 
-const Login: React.FC = () => {
-  const [state] = React.useState({
-    isLoading: false
+const Login: React.FC<Props> = ({ validation }: Props) => {
+  const [state, setState] = React.useState({
+    isLoading: false,
+    email: '',
+    mainError: '',
+    emailError: 'Campo Obrigatório',
+    passwordError: 'Campo Obrigatório'
   })
-  const [errorState] = React.useState({
-    main: '',
-    email: 'Campo Obrigatório',
-    password: 'Campo Obrigatório'
-  })
+  React.useEffect(() => {
+    validation.validate({ email: state.email })
+  }, [state.email])
+
   return (
     <div className={Styles.login}>
       <LoginHeader />
-      <Context.Provider value={ { state, errorState }}>
+      <Context.Provider value={ { state, setState } }>
       <form className={Styles.form}>
         <h2>Login</h2>
         <Input type="email" name="email" placeholder="Digite seu email" />
